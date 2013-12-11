fd765a43a196100eedd550088933be37daabebaa
niketanand123 <niketanand@gmail.com>
2013-12-10 21:56:32 -0800

    retaining job_detail checkbox values

--- app/controllers/job_details_controller.rb
+++ app/controllers/job_details_controller.rb
@@ -10,6 +10,7 @@ class JobDetailsController < ApplicationController
   # GET /job_details/1
   # GET /job_details/1.json
   def show
+    @service_types = ServiceType.all
   end
 
   # GET /job_details/new
@@ -19,6 +20,9 @@ class JobDetailsController < ApplicationController
 
   # GET /job_details/1/edit
   def edit
+    @service_types = ServiceType.all
+    @job_service_types = JobServiceType.where("job_detail_id" =>@job_detail.id)
+    @service_type_ids = @job_service_types.collect{|p| p.service_type_id}
   end
 
   # POST /job_details
@@ -28,6 +32,7 @@ class JobDetailsController < ApplicationController
 
     respond_to do |format|
       if @job_detail.save
+        job_service_type_save
         format.html { redirect_to @job_detail, notice: 'Job detail was successfully created.' }
         format.json { render action: 'show', status: :created, location: @job_detail }
       else
@@ -42,6 +47,7 @@ class JobDetailsController < ApplicationController
   def update
     respond_to do |format|
       if @job_detail.update(job_detail_params)
+        job_service_type_update
         format.html { redirect_to @job_detail, notice: 'Job detail was successfully updated.' }
         format.json { head :no_content }
       else
@@ -69,6 +75,24 @@ class JobDetailsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def job_detail_params
-      params.require(:job_detail).permit(:job_start_date, :job_status_id, :how_many_stories, :existing_roof_type_id, :new_roof_type, :product_type_id, :product_color_id, :sales_rep_id, :estimate_type_id, :contract_price, :deposit_due, :deposit_method, :date_completed, :job_notes, :lead_sheet_note, :info_taken_by_id, :assign_to, :date_taken)
+      params.require(:job_detail).permit(:job_start_date, :job_status_id, :how_many_stories, :existing_roof_type_id, :new_roof_type_id, :product_type_id, :product_color_id, :sales_rep_id, :estimate_type_id, :contract_price, :deposit_due, :deposit_method, :date_completed, :job_notes, :lead_sheet_note, :info_taken_by_id, :assign_to_id, :date_taken)if params[:job_detail]
+    end
+
+    def job_service_type_save
+      params[:service_types_checkbox].each do |check|
+        service_type_id_value = check
+        @job_service_type = JobServiceType.new("job_detail_id" =>@job_detail.id, "service_type_id"=>service_type_id_value)
+        @job_service_type.save
+      end
+    end
+    def job_service_type_update
+      @job_service_types = JobServiceType.where("job_detail_id" =>@job_detail.id)
+      if @job_service_types != nil
+        @job_service_types.destroy_all
+      end
+      params[:service_types_checkbox].each do |check|
+         @job_service_type = JobServiceType.new("job_detail_id" =>@job_detail.id, "service_type_id"=>check.to_f)
+         @job_service_type.save
+      end
     end
 end
