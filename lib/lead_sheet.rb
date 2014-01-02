7a83856de59c6e73bc994b6c5ce3e10d77c560a5
niket <niketanand@gmail.com>
2014-01-02 15:53:38 -0800

    image url setting

--- lib/lead_sheet.rb
+++ lib/lead_sheet.rb
@@ -12,8 +12,8 @@ class LeadSheet
     #pdf.image image_path, :resize => 0.5 #if Rails.env.production?
     ##pdf.image "#{request.protocol}#{request.host_with_port}#{asset_path("banner.png")}", :resize => 0.5 if Rails.env.production?
 
-    pdf.image "#{Rails.root}/app/assets/images/banner.jpg", :resize => 0.5 if Rails.env.development?
-    pdf.image "#{Rails.root}/public/assets/banner-649d828f81646be3eb1f2a3e8ddc008c.jpg", :resize => 0.5 if Rails.env.production?
+    pdf.image "#{Rails.root}/app/assets/images/banner.jpg", :resize => 0.5# if Rails.env.development?
+    #pdf.image "#{Rails.root}/public/assets/banner-649d828f81646be3eb1f2a3e8ddc008c.jpg", :resize => 0.5 if Rails.env.production?
 
 
     pdf.add_text(250, 720, "LEAD SHEET", size=20 )
