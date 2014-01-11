47c82c6a6b706c0f4fce994dc513ff5dec504f43
niket <niketanand@gmail.com>
2014-01-10 23:42:18 -0800

    vendor

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -3,7 +3,7 @@ class Vendor < ActiveRecord::Base
   validates_presence_of :name
   validates_presence_of :address1
   validates_presence_of :zip1
-  alidates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
   validates_format_of :zip1, :with =>  /^\d{5}(-\d{4})?$/, :message => "should be 12345 or 12345-1234",:multiline => true
   validates_format_of :zip2, :with =>  /^\d{5}(-\d{4})?$/, :message => "should be 12345 or 12345-1234",:multiline => true
   validates :gm_phone, :phone_number => {:ten_digits => true, :seven_digits => true, :allow_blank => true, :message => "Phone number must be either seven or digits in length, or blank."}
@@ -15,15 +15,5 @@ class Vendor < ActiveRecord::Base
     self.gm_phone = self.gm_phone.convert_to_phone
     self.sales_rep_phone = self.sales_rep_phone.convert_to_phone
   end
-  #before_destroy :without_dependency
-  #
-  #def without_dependency
-  #  if self.job_tasks_entered_by.size() > 0 || self.job_tasks_completed_by.size() > 0
-  #    self.errors.add :base, "The Employee cannot be deleted because it has job task associated to it"
-  #  end
-  #  if self.job_call_notes.size() > 0
-  #    self.errors.add :base, "The Employee cannot be deleted because it has job call notes associated to it"
-  #  end
-  #  self.errors.blank?
-  #end
+
 end
