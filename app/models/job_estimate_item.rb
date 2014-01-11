4d0236611795af2e6d8a9d7a77237ac09f419c40
niket <niketanand@gmail.com>
2014-01-11 00:15:27 -0800

    asc assembly in estimate

--- app/models/job_estimate_item.rb
+++ app/models/job_estimate_item.rb
@@ -1,6 +1,6 @@
 class JobEstimateItem < ActiveRecord::Base
   belongs_to :job_estimate, :class_name => 'JobEstimate', :foreign_key => :job_estimate_id
-  belongs_to :assembly, :class_name => 'Assembly', :foreign_key => :master_item_id
+  belongs_to :assembly, :class_name => 'Assembly', :foreign_key => :master_item_id, :order => "item_description ASC"
   validates :step, numericality: true, :allow_blank => true
   validates :qty, numericality: true
   validates :retail_price, numericality: true
