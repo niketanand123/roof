fa1fbf699e097ceeee80abbe5f8274dcee9b9fbf
niket <niketanand@gmail.com>
2014-01-28 06:41:39 -0800

    added before_filter in application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,4 +20,9 @@ class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, alert: "Access denied."
   end
+  before_filter do
+    resource = controller_name.singularize.to_sym
+    method = "#{resource}_params"
+    params[resource] &&= send(method) if respond_to?(method, true)
+  end
 end
