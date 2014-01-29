045191f648c175bcb444d2c42994cee040272100
niket <niketanand@gmail.com>
2014-01-29 04:49:12 -0800

    removed allpages

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,10 +12,10 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require jquery-ui
 //= require_tree .
 //= require underscore
 //= require gmaps/google
-//= require jquery-ui
 //= require menu
 jQuery.ajaxSetup({
     'beforeSend': function(xhr) { xhr.setRequestHeader("Accept", "text/javascript") }
