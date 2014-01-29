bc3b6c961bdfeccadd0bb14f64f8274d3db8c910
niket <niketanand@gmail.com>
2014-01-29 04:23:12 -0800

    http mask

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,12 +13,11 @@
 //= require jquery
 //= require jquery_ujs
 //= require allPages
-//= require jquery-ui
 //= require_tree .
 //= require underscore
 //= require gmaps/google
+//= require jquery-ui
 //= require menu
-//= require xmaskedinput-1.3.1.min
 jQuery.ajaxSetup({
     'beforeSend': function(xhr) { xhr.setRequestHeader("Accept", "text/javascript") }
 });
